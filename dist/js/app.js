c8c9c5d92aac9a6f5efddbc7d7116e49d06a2c89
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-17 16:05:30 -0300

    Negociação, modelagem e regras.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-alert('Oi.')
+import { Negociacao } from './models/negociacao.js';
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao); // Exibe a data, a quantidade e o valor, tudo privado.
+negociacao.quantidade = 1000; // Insere a variável dinamicamente em negociacao.
+console.log(negociacao); // Exibe as variáveis privadas e a nova quantidade.
