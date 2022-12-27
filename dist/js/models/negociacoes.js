2cf606d559fc9d465bd650afdd45597389f44236
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-27 19:47:38 -0300

    Revisão.

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -6,7 +6,9 @@ export class Negociacoes {
         this.negociacoes.push(negociacao);
     }
     lista() {
-        // return this.negociacoes // Retorna a lista mesmo.
-        return [...this.negociacoes]; // Retorna cópia da lista.
+        // return [...this.negociacoes] // Retorna cópia da lista.
+        return this.negociacoes; // Retorna a lista mesmo.
+        // Mas como o retorno é do tipo ReadonlyArray, 
+        // a lista da classe permanece imutável.
     }
 }
