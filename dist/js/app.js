6a1df9a491eeff85fa8efed386ef1e003c11db46
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-27 14:26:19 -0300

    Configuração básica do compilador.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,12 @@
 import { Negociacao } from './models/negociacao.js';
-
 const negociacao = new Negociacao(new Date(), 10, 100);
 // console.log(negociacao.data); // data não declarada (está, mas privada).
 // console.log(negociacao.#data); // erro de sintaxe: não acessível.
 // negociacao.data = "Uma data"; // erro de sintaxe: não há setter.
 // daqui pra frente, o código conterá os getters.
-
 console.log(negociacao.data); // Acesso via getter DECLARADO na classe.
-
+console.log(negociacao.quantidade);
+console.log(negociacao.valor);
+console.log(negociacao.volume);
 const negociacao2 = new Negociacao(new Date()); // Faltam parâmetros.
 console.log(negociacao2.volume); // Retorna NaN. Não deveria.
