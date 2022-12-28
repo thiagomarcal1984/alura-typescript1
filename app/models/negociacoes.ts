4c426cac63ec465e80092d3046f315c466ad7187
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-28 10:10:21 -0300

    Arrays e Generics.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    // private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    // lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
         // return [...this.negociacoes] // Retorna cópia da lista.
         return this.negociacoes // Retorna a lista mesmo.
         // Mas como o retorno é do tipo ReadonlyArray, 
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,10 +1,12 @@
 export class Negociacoes {
     constructor() {
+        // private negociacoes: Array<Negociacao> = [];
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // lista(): ReadonlyArray<Negociacao> {
     lista() {
         // return [...this.negociacoes] // Retorna cópia da lista.
         return this.negociacoes; // Retorna a lista mesmo.
